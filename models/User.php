98704dde9f7d8908ae2831db1d9411c4eb137134
floaredesoare <ana.rebeca.crisan@gmail.com>
2022-11-09 14:21:22 +0200

    Added the User and Group Classes

--- models/User.php
+++ models/User.php
@@ -1,5 +1,9 @@
 <?php
+<<<<<<< Updated upstream
 include_once('../../utils/DBConfiguration.php');
+=======
+include_once('../utils/DbConfiguration.php');
+>>>>>>> Stashed changes
 
 
 class User {
@@ -14,7 +18,11 @@ class User {
     
     // Constructor
     function __construct(DbConfiguration $db, string $email,
+<<<<<<< Updated upstream
         string $password, string $first_name, string $last_name, string $prefix){
+=======
+        string $password, string $first_name, string $last_name, ?string $prefix){
+>>>>>>> Stashed changes
         
         $this->db_config = $db;
         $this->email = $email;
@@ -29,9 +37,13 @@ class User {
         
         if(!isset($existing_table->fetch()['TABLE_NAME'])){
             $this->db_config->connection->query($this->createUserTable());
+<<<<<<< Updated upstream
             
             $this->db_config->connection->query($this->insertMockData());
             //echo $this->insertMockData();
+=======
+            $this->db_config->connection->query($this->insertMockData());
+>>>>>>> Stashed changes
         } 
         
     }
@@ -46,20 +58,32 @@ class User {
         return "CREATE TABLE IF NOT EXISTS User(
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     email VARCHAR(20) NOT NULL UNIQUE,
+<<<<<<< Updated upstream
                     password VARCHAR(20) NOT NULL,
+=======
+                    password VARCHAR(100) NOT NULL,
+>>>>>>> Stashed changes
                     first_name VARCHAR(30) NOT NULL,
                     last_name VARCHAR(20) NOT NULL,
                     prefix VARCHAR(10)
             );";
     }
+<<<<<<< Updated upstream
     
     
     
+=======
+
+>>>>>>> Stashed changes
     private function insertMockData(){
         $sql__insert_stmt = "INSERT IGNORE INTO User(email, password, first_name, last_name, prefix) values ";
         
         // Adding Student accounts
+<<<<<<< Updated upstream
         for($ind=1; $ind<=200; $ind++){ 
+=======
+        for($ind=1; $ind<=600; $ind++){ 
+>>>>>>> Stashed changes
             $email = "student" . $ind . "@studmail.com";
             $password = md5($this->createRandomPassword());
             $first_name = "First" . $ind;
@@ -87,6 +111,7 @@ class User {
         
         // Adding Admin Account
         $password = md5($this->createRandomPassword());
+<<<<<<< Updated upstream
         $sql__insert_stmt = $sql__insert_stmt .  "('admin@studmail.com', '" . $password . "', 'FN', 'LN', '');";
         
        
@@ -95,6 +120,15 @@ class User {
     }
     
     
+=======
+        $sql__insert_stmt = $sql__insert_stmt .  "('admin@studmail.com', '" . $password . "', 'FN', 'LN', '')";
+        
+        
+        return $sql__insert_stmt; 
+        
+    }
+    
+>>>>>>> Stashed changes
     private static function createRandomPassword() {
         $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
         $pass = array(); //remember to declare $pass as an array
@@ -107,7 +141,31 @@ class User {
     }
     
     
+<<<<<<< Updated upstream
     
+=======
+    function verifyUser($email, $password){
+        // Checks that the credentials are valid
+        // Returns the user if it can be found
+        
+        $query = "SELECT * FROM `user` WHERE 
+        `user`.email = '" . $email . "' AND 
+        `user`.`password` = '" . md5($password) . "';";
+        
+        $user = $this->db_config->connection->query($query);
+        $found = $user->fetch();
+        if (isset($found['email'])){
+            $result = new User($this->db_config, $found['email'], $found['password'],
+                $found['first_name'], $found['last_name'], $found['prefix']);
+            $result->set_id($found['id']);
+            return $result;
+        }
+        
+        return null;
+        
+    }
+   
+>>>>>>> Stashed changes
     
     // Getters and Setters
     function set_id($id) {
@@ -147,8 +205,11 @@ class User {
         return $this->prefix;
     }
 
+<<<<<<< Updated upstream
  
     // Other Methods
+=======
+>>>>>>> Stashed changes
     
 
 }
