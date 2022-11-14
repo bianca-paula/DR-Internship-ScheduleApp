22a9336a24935e5a47aa95fa0a63eeb8f0007e73
Samuel Eke <samuel2000eke@yahoo.com>
2022-11-14 17:18:38 +0200

    Loaded groups from array into html tags

--- views/admin/assigned-to.php
+++ views/admin/assigned-to.php
@@ -1,3 +1,10 @@
+<?php
+
+$groups = array('231', '232', '233', '234', '235', '236', '237');
+
+?>
+
+
 <div class="col-lg-2 my-5 mx-2 pb-0 assigned-block">
     <div class="row form-container">
         <div class="col">
@@ -10,251 +17,20 @@
                 <!-- Form -->
                 <div class="col px-0 ">
                     <form class="assigned-block-form">
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="233">
-                            </div>
 
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
+                        <?php
+                        foreach ($groups as $group) {
+                        ?>
+                            <div class="row ">
+                                <div class="col d-flex justify-content-end p-0 my-2">
+                                    <input type="checkbox" id="<?php print($group) ?>">
+                                </div>
 
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
+                                <div class="col d-flex justify-content-start align-items-center">
+                                    <label for="<?php print($group) ?>"><?php print($group) ?></label>
+                                </div>
                             </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-
-                        <div class="row ">
-                            <div class="col d-flex justify-content-end p-0 my-2">
-                                <input type="checkbox" id="232">
-                            </div>
-
-                            <div class="col d-flex justify-content-start align-items-center">
-                                <label for="232">232</label>
-                            </div>
-                        </div>
-
+                        <?php } ?>
                     </form>
                 </div>
             </div>
