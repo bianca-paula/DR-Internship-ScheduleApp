372a36b5dc1fc2088f6ac1afa86f1a1d69b35c95
Samuel Eke <samuel2000eke@yahoo.com>
2022-11-16 15:56:55 +0200

    see scheduled table generated with php code

--- views/admin/table.php
+++ views/admin/table.php
@@ -1,124 +1,52 @@
-  <table class="w-100" data-toggle="table" id="schedule-table">
-      <thead>
+<table class="w-100" data-toggle="table" id="schedule-table">
+    <thead>
+        <tr class="table-head">
+            <th data-width="80"></th>
+            <?php
+            const WEEKDAYS = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday');
+            foreach (WEEKDAYS as $day) {
+            ?>
+                <th data-width="150"><?php print($day) ?></th>
+            <?php
+            }
+            ?>
+        </tr>
+    </thead>
+    <tbody class="table-data">
+        <?php
+        for ($hour = 8; $hour <= 19; $hour++) {
+        ?>
+            <tr>
+                <th><?php print $hour; ?>-<?php print($hour + 1) ?></th>
+                <?php
 
-          <tr data-height="1" class="table-head">
-              <th data-field="hour" data-width="80">
-              </th>
-              <th data-field="monday" data-width="150">Monday</th>
-              <th data-field="tuesday" data-width="150">Tuesday</th>
-              <th data-field="wednesday" data-width="150">Wednesday</th>
-              <th data-field="thursday" data-width="150">Thursday</th>
-              <th data-field="friday" data-width="150">Friday</th>
-          </tr>
-      </thead>
-      <tr>
-          <th>8-9</th>
-          <td data-row-id="1" data-toggle="modal" data-target="#courseModal"></td>
-          <td data-row-id="2" data-toggle="modal" data-target="#courseModal"></td>
-          <td data-row-id="3" data-toggle="modal" data-target="#courseModal">ISS</td>
-          <td data-row-id="4" data-toggle="modal" data-target="#courseModal"></td>
-          <td data-row-id="5" data-toggle="modal" data-target="#courseModal">OS</td>
-      </tr>
-      <tr>
-          <th>9-10</th>
-          <td class="table-data" data-row-id="6" data-toggle="modal" data-target="#courseModal">
-              <div class="container">
-                  <div class="row justify-content-end">
-                      <div class="col-4 justify-content-end">AI</div>
-                      <div class="col-4 justify-content-center">
-                          <form action="#" method="post">
-                              <button type="button" class="col mx-auto delete-button" data-bs-toggle="modal" data-bs-target="delete-modal">
-                                  <i class="fa-solid fa-trash-can"></i>
-                              </button>
-                          </form>
-                      </div>
-                  </div>
-              </div>
+                foreach (WEEKDAYS as $day) {
+                ?>
+                    <td data-toggle="modal" data-target="#courseModal">
+                        <div class="container">
+                            <div class="row justify-content-end">
+                                <div class="col-4 justify-content-end">AI</div>
+                                <div class="col-4 justify-content-center">
+                                    <form action="#" method="post">
+                                        <button type="button" class="col mx-auto delete-button" data-bs-toggle="modal" data-bs-target="delete-modal">
+                                            <i class="fa-solid fa-trash-can"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </td>
+                <?php } ?>
+            </tr>
+        <?php
+        } ?>
+    </tbody>
+</table>
 
-          </td>
-          <td data-row-id="7" data-toggle="modal" data-target="#courseModal">ASC</td>
-          <td data-row-id="8" data-toggle="modal" data-target="#courseModal"></td>
-          <td data-row-id="9" data-toggle="modal" data-target="#courseModal">OS</td>
-          <td data-row-id="10" data-toggle="modal" data-target="#courseModal"></td>
-      </tr>
-      <tr>
-          <th>10-11</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-      <tr>
-          <th>11-12</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-      <tr>
-          <th>12-13</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-      <tr>
-          <th>13-14</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-      <tr>
-          <th>14-15</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-      <tr>
-          <th>15-16</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-      <tr>
-          <th>16-17</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-      <tr>
-          <th>17-18</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-      <tr>
-          <th>18-19</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-      <tr>
-          <th>19-20</th>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-      </tr>
-  </table>
+<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/bootstrap-table@1.21.1/dist/bootstrap-table.min.js"></script>
+<script src="https://kit.fontawesome.com/aca3ebed9c.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
+<script src="https://unpkg.com/jspdf-autotable@3.5.22/dist/jspdf.plugin.autotable.js"></script> -->
