7872cd7596445446b90ffd65ec5cdcffb503262b
Samuel Eke <samuel2000eke@yahoo.com>
2022-11-18 14:03:40 +0200

    delete and add course functionality in view2

--- views/page-parts/header.php
+++ views/page-parts/header.php
@@ -2,34 +2,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- jQuery CDN -->
-    <!-- <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script> -->
-
-    <!-- Bootstrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
-    <!-- Fontawesome -->
-    <script src="https://kit.fontawesome.com/57ff89206e.js" crossorigin="anonymous"></script>
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;500;600;700;900&family=Righteous&display=swap" rel="stylesheet">
-    <!-- CSS stylesheet -->
-    <link rel="stylesheet" href="../../assets/style/style.css">
-
-    <link rel="stylesheet" href="../../assets/CSS/admin/admin.css">
-
-    <title>Profesor</title>
+    <!-- Bootstrap and Bootstrap Table -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.21.1/dist/bootstrap-table.min.css">
+    <!-- Google Fonts && Font Awesome -->
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./assets/style/style.css">
 
 </head>
 <div class="container-fluid">
-    <div class="row header-container d-flex align-items-center justify-content-center py-3">
-        <div class="col-6 d-flex align-items-center justify-content-start ps-4 py-auto">
-            <h3><img src="../../assets/Images/logo_books.png" alt="Logo Books" height="40em">&nbsp;ScheduleApp</h3>
+    <header>
+        <div class="row header-container py-3">
+            <div class="col-4 pl-5">
+                <h3><img src="./assets/images/logo_books.png" alt="Logo Books" height="40em">&nbsp;ScheduleApp</h3>
+            </div>
+            <div class="col-4"></div>
+            <div class="col-4 pr-5">
+                <h3 class="float-right">Username &nbsp;<span onclick="logoutPage()"><i class="fa-solid fa-right-from-bracket"></span></i></h3>
+            </div>
         </div>
-
-        <div class="col-6 d-flex align-items-center justify-content-end pe-4 py-auto">
-            <h3 class="">Username &nbsp;<span onclick="logoutPage()"><i class="fa-solid fa-right-from-bracket"></span></i></h3>
-        </div>
-    </div>
+    </header>
 </div>
